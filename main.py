9bbd47aad05224ea58cc3b4adb3ddeca2ebebdef
Arnaud <a-debroissia@live.fr>
2018-07-02 08:41:36 +0200

    modifications for better results

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import scipy.misc
+import time
 
 import os
 
@@ -17,6 +18,8 @@ with sess.as_default():
     run_metadata = tf.RunMetadata()
 
     wk, cl = nbp.create_workflow(None, [["load_dataset", {"batchsize" : 128, "dataset" : "celebA" , "resize_dim" : [128,128], "central_crop" : True}],
+                                        ["bufferize_dataset"],
+                                        ["stage_dataset"],
                                         ["input_layer", {"new_input": [128,1,1,128]}],
                                         ["tf.random_normal"],
                                         ["tf.nn.l2_normalize", {"axis" : -1}],
@@ -25,30 +28,34 @@ with sess.as_default():
                                         ["network", ["discriminator", "discriminator_128_128"]],
                                         ["bridge_layer", {"bridge_name": "discr_fake"}],
                                         ["set_adam_optimizer", {"learning_rate" : 0.0002, "beta" : 0.5}],
-                                        ["softmax_loss", {"labels": 0.0}],
+                                        ["squared_error_1", [0.0]],
                                         ["compute_gradients", {"scopes" : ["discriminator"], "clear_losses" : True}],
                                         ["trainer"],
                                         ["save_in_list", {"list_name" : "trainers"}],
                                         ["clear_field", {"field_name" : "gradients"}],
-                                        ["input_layer", {"new_input": "@:/dataset/image"}],
+                                        ["input_layer", {"new_input": "@:/data_inputs/image"}],
                                         ["network", ["discriminator"]],
                                         ["bridge_layer", {"bridge_name": "discr_true"}],
-                                        ["softmax_loss", {"labels" : 1.0}],
+                                        ["squared_error_1", [1.0]],
                                         ["compute_gradients", {"scopes": ["discriminator"], "clear_losses": True}],
                                         ["trainer"],
+                                        ["feed_stager"],
                                         ["save_in_list", {"list_name": "trainers"}],
                                         ["clear_field", {"field_name": "gradients"}],
                                         ["set_adam_optimizer", {"learning_rate" : 0.0002, "beta" : 0.5}],
                                         ["input_layer", {"new_input": "@:/bridges/discr_fake"}],
-                                        ["softmax_loss", {"labels": 1.0}],
+                                        ["squared_error_1", [1.0]],
                                         ["compute_gradients", {"scopes": ["generator"], "clear_losses": True}],
                                         ["trainer"],
                                         ["save_in_list", {"list_name": "trainers"}],
+                                        ["save_in_list", {"list_name": "trainers"}],
                                         ["clear_field", {"field_name": "gradients"}],
-                                        ["initializer"]], "basic_gan_training")
+                                        ["initializer"],
+                                        ["saver"]], "basic_gan_training")
     #plt.ion()
+    starting_time=time.time()
     for i in range(1000000):
-        if i%250==0:
+        if i%251==0:
             sess.run(cl["trainers"][i%len(cl["trainers"])], options=options, run_metadata=run_metadata)
             fetched_timeline = timeline.Timeline(run_metadata.step_stats)
             chrome_trace = fetched_timeline.generate_chrome_trace_format()
@@ -57,11 +64,12 @@ with sess.as_default():
         else:
             sess.run(cl["trainers"][i%len(cl["trainers"])])
         if i%1000==0:
-            print(i)
+            print(i,  round(time.time()-starting_time,2))
             img, res = sess.run([cl["dataset"]["image"], cl["bridges"]["generated_images"]])
             #scipy.misc.imsave('generated.jpg', res[0])
-            scipy.misc.toimage(res[0], cmin=0.0, cmax=1.0).save('generated.jpg')
+            scipy.misc.toimage(np.hstack((np.vstack((res[0], res[1])),np.vstack((res[2], res[3])))), cmin=-1.0, cmax=1.0).save('generated.jpg')
             os.system('tiv -w 128 generated.jpg')
+            cl["saver"](i)
             #plt.cla()
             #plt.imshow(np.vstack((img[0], res[0])))
             #plt.show()
